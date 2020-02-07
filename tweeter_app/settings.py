5c70a79abe174853fa39f14f7c93627c74577e07
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2020-02-07 12:41:18 -0600

    added bootstrap

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'users',
     'tweets',
 ]
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'tweeter_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
